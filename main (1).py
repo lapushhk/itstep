86c39b029ef8aefe0f44d8e0fc842197355b3562
lapushhk <147985320+lapushhk@users.noreply.github.com>
2023-10-28 20:25:11 +0300

    Add files via upload

---
+++ main (1).py
@@ -0,0 +1,15 @@
+import random
+
+def function():
+  v = imt(input('vvedit naibilshe\n'))
+  m = imt(input('vvedit naimenshe\n'))
+  list_1 = list
+  list_1.append (m)
+  a = m
+  while a<v:
+    a+1
+    list_1.append (a)
+    print(list_1)
+    r = random.choice(list_1)
+    print(r)
+  functoin()
